980aa2134b2a64ea96b8d4dc14526b11c1ac1b16
Sydney Idundun <76040196+SydneyTechnologies@users.noreply.github.com>
2025-01-27 23:24:20 +0400

    feat: Updating some fields on the yacht schema

--- schema/yachts.ts
+++ schema/yachts.ts
@@ -12,24 +12,43 @@ export default defineType({
       title: 'Name',
       validation: (Rule) => Rule.required(),
     }),
+
     defineField({
       name: 'slug',
       type: 'slug',
       title: 'Slug',
       options: {source: 'name'},
     }),
+
+    defineField({
+      name: 'shortDescription',
+      type: 'text',
+      title: 'Short Description',
+      description: 'Short description of the yacht',
+    }),
+
+    defineField({
+      name: 'moreDetails',
+      type: 'text',
+      title: 'More Details',
+      description:
+        'More details about the yacht. This will be displayed on the details page of the yacht',
+    }),
+
     defineField({
       name: 'length',
       type: 'number',
       title: 'Vessel Length',
       description: 'Length of the vessel in ft',
     }),
+
     defineField({
       name: 'capacity',
       type: 'number',
       title: 'Vessel Capacity',
       description: 'Number of people the vessel can accommodate',
     }),
+
     defineField({
       name: 'buildDate',
       type: 'date',
@@ -45,6 +64,7 @@ export default defineType({
       title: 'Price',
       description: 'Price of the vessel',
     }),
+
     defineField({
       name: 'cabins',
       type: 'number',
@@ -53,5 +73,12 @@ export default defineType({
     }),
 
     defineField({name: 'image', type: 'image-type', title: 'Image', options: {hotspot: true}}),
+    defineField({
+      name: 'gallery',
+      type: 'array',
+      title: 'Gallery',
+      of: [{type: 'reference', to: [{type: 'image-type'}]}],
+      description: 'Gallery of images of the yacht',
+    }),
   ],
 })
